bfe874a996c471433b270e2ec08805c2c428686e
Alejo D'Aloia <alejodaloia03@gmail.com>
2025-05-21 21:42:31 -0300

    agregada la wallet

--- Programacion/Contador_de_Clicks-main/Contador de Cliks/contador de clicks.js
+++ Programacion/Contador_de_Clicks-main/Contador de Cliks/contador de clicks.js
@@ -1,5 +1,6 @@
 const texto= document.getElementById("texto");
 var audio= document.getElementById("audio");
+const video= document.getElementById("screamer");
 
 num= 0;
 parseInt(texto);
@@ -9,6 +10,16 @@ function contador(){
         audio.play();
     } else{
         audio.currentTime= 0;
+    }
+    if(texto.innerHTML == 15){
+        video.style.display= "show";
+        video.play();
+        setTimeout(function(){
+            video.pause();
+            video.currentTime= 0;
+        }, 5000);
+        texto.innerHTML= "¡Felicidades! Has llegado a 15 clicks.";
+        return;
     }
     num+=1;
     texto.innerHTML= num;
